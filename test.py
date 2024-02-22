d4e58ebe337136c6be1b235cd75a203d2b24f086
J2S80000 <jessystal28@gmail.com>
2024-02-22 22:24:19 +0100

    v2 du test frro

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-nom = input("Bonjour ! Quel est votre nom ? ")
+nom = input("Bonjour ! Quel est votre nom meccccc ? ")
 print("Bonjour, {} ! Bienvenue dans le monde de la programmation en Python!".format(nom))
